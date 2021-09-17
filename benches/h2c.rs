8aff68d55333199611e55f8091d93fe6157c7a2f
armfazh <armfazh@cloudflare.com>
2021-09-16 21:27:03 -0700

    Updated benchmarks.

--- benches/h2c.rs
+++ benches/h2c.rs
@@ -1,32 +1,33 @@
-extern crate num_bigint;
-
-use criterion::{criterion_group, criterion_main, Benchmark, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 use h2c_rust_ref::{
-    GetHashToCurve, P256_XMDSHA256_SSWU_RO_, P384_XMDSHA512_SSWU_RO_, P521_XMDSHA512_SSWU_RO_,
+    GetHashToCurve, P256_XMDSHA256_SSWU_NU_, P256_XMDSHA256_SSWU_RO_, P384_XMDSHA384_SSWU_NU_,
+    P384_XMDSHA384_SSWU_RO_, P521_XMDSHA512_SSWU_NU_, P521_XMDSHA512_SSWU_RO_,
 };
 
 fn h2c(c: &mut Criterion) {
     let msg = b"message to be hashed";
     let dst = b"domain separation tag";
+
+    let mut group = c.benchmark_group("Suite");
+    group.sample_size(10);
+
     for suite in [
         P256_XMDSHA256_SSWU_RO_,
-        P384_XMDSHA512_SSWU_RO_,
+        P384_XMDSHA384_SSWU_RO_,
         P521_XMDSHA512_SSWU_RO_,
+        P256_XMDSHA256_SSWU_NU_,
+        P384_XMDSHA384_SSWU_NU_,
+        P521_XMDSHA512_SSWU_NU_,
     ]
     .iter()
     {
         let h = suite.get(dst);
-        c.bench(
-            format!("{}", suite).as_str(),
-            Benchmark::new("hash", move |b| {
-                b.iter(|| {
-                    h.hash(msg);
-                })
-            })
-            .sample_size(10),
-        );
+        group.bench_function(format!("{}", suite).as_str(), move |b| {
+            b.iter(|| h.hash(msg))
+        });
     }
+    group.finish()
 }
 
 criterion_group!(h2c_bench, h2c);
